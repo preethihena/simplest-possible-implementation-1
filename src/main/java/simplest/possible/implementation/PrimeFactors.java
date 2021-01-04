66befac523bb6f3db7d49b1f95c6dedd4525e55f
Hena Dindigalla <henadindigalla@Henas-MacBook-Pro.local>
2021-01-04 16:50:48 +0530

    changes made to PrimeFactors.java

--- src/main/java/simplest/possible/implementation/PrimeFactors.java
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -2,17 +2,15 @@ package simplest.possible.implementation;
 import java.util.*;
 
 public class PrimeFactors {
-    public  List<String> of( int  number ) {
-        List<String> primeFactorsList = Collections.emptyList();
-        if( isOne( number ) )
+    public  List<Integer> of(int  number) {
+        List<Integer> primeFactorsList = Collections.emptyList();
+        if(isOne(number))
             return Collections.emptyList();
         return primeFactorsList;
 
     }
     public Boolean isOne( int number )
     {
-        if( number == 1 )
-            return true;
-        return false;
+        return number == 1;
     }
 }
